8f01bcdbacc92b87fbe8dab101e339caee0313a8
Paige Ponzeka <paige@disrupto.com>
2011-04-17 00:27:06 -0400

    added back buttons to xib files and also included previously missing controllers etc

---
+++ Tester/CreateVoteDestinationsViewController.h
@@ -0,0 +1,16 @@
+//
+//  CreateVoteDestinationsViewController.h
+//  Tester
+//
+//  Created by Paige Ponzeka on 4/15/11.
+//  Copyright 2011 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CreateVoteDestinationsViewController : UIViewController {
+    
+}
+
+@end
---
+++ Tester/CreateVoteUsersViewController.h
@@ -0,0 +1,16 @@
+//
+//  CreateVoteUsersViewController.h
+//  Tester
+//
+//  Created by Paige Ponzeka on 4/15/11.
+//  Copyright 2011 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CreateVoteUsersViewController : UIViewController {
+    
+}
+
+@end
